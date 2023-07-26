4ed8111621c3e1bf5b91e9cade35a5e507ae09c2
BilalAbouBaraki <bilalaboub1452001@gmail.com>
2023-07-26 18:37:48 +0300

    commit message Calender

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
 
+
+
+const MainContainer=()=>{
+return(
+ <>
+ </>
+
+);
+};
+function CreateDiv(){
+  let newdiv=document.createElement("div")
+  newdiv.className='ContainerStyle1'
+  MainContainer.
+}
+const Button1=()=>{
+  return(
+  <button onclick={CreateDiv()}></button>
+  )
+}
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <div className="ContainerStyle"><MainContainer/></div>
+    <Button1/>
+    </>
+  
   );
 }
 
